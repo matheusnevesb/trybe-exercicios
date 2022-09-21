2eeb412aaaeea866f92cf1c4a9a4eb455e3f382f
matheus <matheusnbbg@hotmail.com>
2022-09-21 15:05:34 -0300

    variaveis - para fixar

---
+++ secoes/4.introducao-ao-JavaScript/dia-1/variaveis/scripts.js
@@ -0,0 +1,12 @@
+const myName = "matheus";
+
+const birthCity = "Recife";
+
+let birthYear = 1997;
+
+birthYear = 2030;
+birthCity = "outra cidade";
+
+console.log(myName)
+console.log(birthYear)
+console.log(birthCity)
