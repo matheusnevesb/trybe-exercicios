97974041db652e7bd420773a66c4f9aee58b3ff6
matheus <matheusnbbg@hotmail.com>
2022-09-24 19:37:08 -0300

    adiciona exercicio do dia 1

---
+++ secoes/4.introducao-ao-JavaScript/dia-1/exercicio/scripts.js
@@ -0,0 +1,23 @@
+let a = 1;
+let b = 2;
+
+adicao = a + b;
+subtracao = a - b;
+multiplicacao = a * b;
+divisao = a / b;
+modulo = a % b;
+
+// 5
+// Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.
+//Um ângulo será considerado inválido se não tiver um valor positivo.
+// Para os ângulos serem de um triângulo válido, a soma dos três deve ser 180 graus.
+
+const lado1 = 60;
+const lado2 = 60;
+const lado3 = 60;
+
+soma = lado1 + lado2 + lado3;
+
+if (soma == 180) {
+
+}
