5f3ad8fa645f0bef8f0a0b3345b67aae62f0a4a7
matheus <matheusnbbg@hotmail.com>
2022-09-28 15:51:28 -0300

    aula 5.1 seletores de elementos exercicio 1

---
+++ secoes/5.JavaScript:DOM,Eventos-e-Web-Storage/dia-01/Seletores-de-elementos/getElementById.js
@@ -0,0 +1,12 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+let titulo = document.getElementById('page-title')
+titulo.innerText = 'Harry Potter'
+
+let segundoParagrafo = document.getElementById('second-paragraph')
+segundoParagrafo.style.color = 'blue'
+segundoParagrafo.style.backgroundColor = 'gray'
+
+let subTitulo = document.getElementById('subtitle')
+subTitulo.innerText = 'alterado paranovo subtitulo ...'
