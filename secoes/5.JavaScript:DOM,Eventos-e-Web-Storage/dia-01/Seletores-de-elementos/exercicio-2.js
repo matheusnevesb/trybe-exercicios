d8ce12ce02a4108fe174000bf214733eeed6f75d
matheus <matheusnbbg@hotmail.com>
2022-09-28 15:59:34 -0300

    aula 5.1 seletores de elementos exercicio 2

---
+++ secoes/5.JavaScript:DOM,Eventos-e-Web-Storage/dia-01/Seletores-de-elementos/exercicio-2.js
@@ -0,0 +1,9 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+let igual = document.getElementsByClassName('igual');
+paragrado1 = igual[0];
+paragrado1.style.fontSize = '40px'
+
+let subtitulo = document.getElementsByTagName('h4');
+subtitulo[0].style.color = 'green';
