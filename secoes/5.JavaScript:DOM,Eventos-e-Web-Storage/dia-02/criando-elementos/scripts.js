1d2586871094b1064cfa6ec422f550df768cacd9
matheus <matheusnbbg@hotmail.com>
2022-09-29 15:40:59 -0300

    aula 5.2 exercicio criando elementos

---
+++ secoes/5.JavaScript:DOM,Eventos-e-Web-Storage/dia-02/criando-elementos/scripts.js
@@ -0,0 +1,24 @@
+let elementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+
+let primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
+
+let terceiroFilho = document.querySelector('#terceiroFilho');
+
+function criarFilho(ondeVaiSerAdicionado) {
+    novoElement = document.createElement('h1')
+    novoElement.innerText = 'novo elemento'
+
+    ondeVaiSerAdicionado.appendChild(novoElement)
+}
+
+function criarIrmao(elementoIrmao){
+    let novoElemento = document.createElement('h2');
+    novoElemento.innerText = 'Irmao!'
+
+    let paiDele = elementoIrmao.parentElement;
+    paiDele.appendChild(novoElemento)
+}
+
+criarFilho(elementoOndeVoceEsta);
+criarFilho(primeiroFilhoDoFilho);
+criarIrmao(elementoOndeVoceEsta);
