1543a9d9a1f72c711b27df08110bd76eb7d59f1e
matheus <matheusnbbg@hotmail.com>
2022-10-28 14:30:06 -0300

    Dia 8.5 - Spread Operator: para fixar

---
+++ secoes/8.Higher-Order-Functions-do-JavaScript-ES6/dia-05/spread-operator.js
@@ -0,0 +1,12 @@
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['banana', 'maca', 'pera'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['granola', 'aveia', 'mel'];
+
+const fruitSalad = (fruit, additional) => {
+  // Escreva sua função aqui
+  return [...fruit, ...additional]
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
